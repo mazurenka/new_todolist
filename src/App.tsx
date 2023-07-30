ca12bcbcd2ecf71c77aba43c5b71875eb304fd07
mazurenka <mazurenka.pl@gmail.com>
2023-07-30 20:24:35 +0200

    1

--- src/App.tsx
+++ src/App.tsx
@@ -3,24 +3,28 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Todolist/>
+        </div>
+    );
+}
+
+function Todolist() {
+    return (
+        <div>
+            <h3>What to learn</h3>
+            <div>
+                <input/>
+                <button>+</button>
+            </div>
+            <ul>
+                <li><input type="checkbox" checked={true}/>HTML</li>
+                <li><input type="checkbox" checked={true}/>CSS</li>
+                <li><input type="checkbox" checked={false}/>React</li>
+            </ul>
+        </div>
+    )
 }
 
 export default App;
